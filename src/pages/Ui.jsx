e634c1b1a5361f8974a4c68ce7e77cac015e1842
Legimmort82 <144704252+Legimmort82@users.noreply.github.com>
2023-10-19 22:30:59 +0330

    updated
    updated

--- src/pages/Ui.jsx
+++ src/pages/Ui.jsx
@@ -7,9 +7,9 @@ import Button from "../components/UI/Button";
 import Input from "../components/Input/Input";
 import InputCard from "../components/Input/InputCard";
 import Icon from "../components/UI/Icon/Icon";
-import attach from '../../src/assets/images/attach-outline.svg';
-import heart from '../../src/assets/images/heart-outline.svg';
-import message from '../../src/assets/images/message-square-outline.svg';
+import attach from "../../src/assets/images/attach-outline.svg";
+import heart from "../../src/assets/images/heart-outline.svg";
+import message from "../../src/assets/images/message-square-outline.svg";
 import Priorities from "../components/Priority/Priorities";
 const Usersimages = [
   {
@@ -23,43 +23,40 @@ const Usersimages = [
     className: "h-[30px] w-[30px] -ml-[13px]",
   },
   {
-    id:3,
+    id: 3,
     src: Usericon3,
     className: "h-[30px] w-[30px] -ml-[13px]",
   },
   {
-    id:4,
+    id: 4,
     src: AddUser,
     className: "h-[30px] w-[30px] -ml-[13px]",
   },
-]
+];
 function Ui() {
-  const IconList=[
+  const IconList = [
     {
       url: attach,
-      count: 255
+      count: 255,
     },
     {
-      url:heart,
-      count:255
+      url: heart,
+      count: 255,
     },
     {
-      url:message,
-      count:255
-    }
-  ]
+      url: message,
+      count: 255,
+    },
+  ];
   return (
     <div>
-      <Users UsersList={Usersimages}/>
+      <Users UsersList={Usersimages} />
       <Button>ورود</Button>
       <Input />
       <InputCard />
-<Add-Icon
-      {
-        IconList.map((item,index)=><Icon key={index} Icon={item.url} cont={item.count} />)
-      }/>
-      <Priorities color="blue"/>
-
+      <Add-Icon />
+      {/* IconList.map((item,index)=><Icon key={index} Icon={item.url} cont={item.count} />) */}
+      <Priorities color="blue" />
     </div>
   );
 }
