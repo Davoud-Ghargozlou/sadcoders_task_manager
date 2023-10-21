a04133e3390274cdb7819e4ca7e24805b851f9ec
Davoud Qaragouzlou <davoodgh0251@gmail.com>
2023-10-21 11:32:11 +0430

    creating header completed

--- src/pages/Ui.jsx
+++ src/pages/Ui.jsx
@@ -2,10 +2,12 @@ import Users from "../components/Users/Users";
 import Button from "../components/UI/Button";
 import Input from "../components/Input/Input";
 import InputCard from "../components/Input/InputCard";
+import Header from "../components/Header/Header";
 
 function Ui() {
   return (
     <div>
+      <Header />
       <Users />
       <Button>ورود</Button>
       <Input />
