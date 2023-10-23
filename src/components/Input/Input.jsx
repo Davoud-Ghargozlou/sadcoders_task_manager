109cd6d10d87cc8af4b6a8b97842193e9c7bd8d3
AliKhe85 <khadm3359@gmail.com>
2023-10-23 16:32:28 +0330

    change login
    change login

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,43 +2,41 @@ import Button from "../UI/Button";
 import InputCard from "./InputCard";
 
 const Info = [
-  { name: "Username", placeholder: "Your username",type:"text" },
+  { name: "Username",type:"text" },
   { name: "Password", placeholder: "Your password",type:"password" },
 ];
 
 function Input() {
-  return (
-    <div className="flex w-screen h-screen justify-center items-center">
-    <section className="container w-100 h-80 flex flex-col py-6 px-4">
-      <header className="m-auto pb-6" >
-        <img src="public/Logo.svg"/>
-      </header>
-      {Info.map((item) => {
-        return (
-          <InputCard
-            key={item.name}
-            text={item.name}
-            placeholder={item.placeholder}
-            type={item.type}
-          >
-            <img src="/public/Group.svg" />
-          </InputCard>
-        );
-      })}
-      <h3 className="text-xs font-medium m-0 not-italic text-gray-500">
-        forget password?
-      </h3>
-      <div className="flex justify-end items-center">
-      <Button variant="fill" size="sm">
-      Sign up
-      </Button>
-      <Button variant="outline" size="md">
-      Login
-      </Button>
-       </div>
-    </section>
-    </div>
-  );
+
+    return (
+      <div className="flex flex-col items-center py-[2rem] ">
+        <img src="public/Logo (1).svg" className="pb-[56px]"/>
+    <div className="w-[1170px] h-[550px]  flex " >
+      
+      <section className=" w-[585px] h-[550px] flex flex-col bg-gray-300 pb-[79px] pt-[75px] pl-[92px] pr-[92px] basis-1/2	 ">
+        <h1 className="text-[30px] font-bold leading-[38px] text-[#212B36] pb-[40px]">Sign In</h1>
+        {Info.map((item) => {
+          return(
+              <InputCard key={item.name}  text={item.name} placeholder={item.placeholder}>
+                 
+              </InputCard>
+          )
+        })}
+        <button className="w-[400px] bg-[#3758F9] rounded-[6px] h-[50px] py-[13px] px-[28px]">
+          Sign In
+        </button>
+        <div className="flex items-center pt-[40px]">
+        <h3 className="text-[16px] font-medium m-0 not-italic text-[#637381]">forget password?</h3>
+        <div className="flex items-center">
+          <h3 className="text-[16px] text-[#637381] pl-[23px]">Don't have an account?</h3>
+          <a href="#" className="text-[#3758F9]"> Sign Up</a>
+        </div>
+        </div>
+      </section>
+      <div className=" basis-1/2	bg-[#3758F9]"></div>
+      </div>
+      </div>
+    );
 }
 
 export default Input;
