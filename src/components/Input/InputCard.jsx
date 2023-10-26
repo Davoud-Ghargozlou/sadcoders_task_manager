9d238c253a91a61e11f8f90fd908985eda7eacce
Legimmort82 <144704252+Legimmort82@users.noreply.github.com>
2023-10-26 23:21:29 +0330

    login linked

--- src/components/Input/InputCard.jsx
+++ src/components/Input/InputCard.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import PropType from "prop-types"
 
-function InputCard({ text, placeholder,type="text" }) {
+function InputCard({ text,value, placeholder,type="text",onChange }) {
   return (
     <div className="bg-gray-300 pb-[20px]">
       <p className="text-[16px] font-medium text-left text-[#111928] m-0 pb-[10px]">{text}</p>
       <div className="flex justify-between items-center py-3 pr-4 pl-5 bg-white rounded-md">
-        <input type={type} placeholder={placeholder} className="w-[400px] border-none outline-none" />
+        <input type={type} value={value} onChange={onChange} placeholder={placeholder} className="w-[400px] border-none outline-none" />
         {type==="password" && <img src="public/Group.svg" />}
       </div>
     </div>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,34 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
 import Auth from "@/pages/Auth";
 import Ui from "@/pages/Ui";
-import Input from "@/components/Input/Input";
 import Home from "@/pages/index";
 import P404 from "@/pages/P404";
 import TaskPage from "@/pages/TaskPage";
+import Layout from "@/components/Layout";
 const routes = [
   {
-    path: "/Login",
-    element: <Input />,
-  },
-  {
-    path: "/Auth",
-    element: <Auth />,
-  },
-  {
     path: "/",
-    element: <Home/>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/Auth",
+        element: <Auth />,
+      },
+      {
+        path: "/ui",
+        element: <Ui />,
+      },
+      {
+        path: "/Task",
+        element: <TaskPage />,
+      },
+    ],
   },
   {
-    path: "/ui",
-    element: <Ui />,
-  },
-  {
-    path:"*",
-    element:<P404/>
-  },
-  {
-    path: "/Task",
-    element: <TaskPage/>
+    path: "*",
+    element: <P404 />,
   },
 ];
 const router = createBrowserRouter(routes);
