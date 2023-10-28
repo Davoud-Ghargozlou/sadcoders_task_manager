6460b3d5c196ed3ef2a22ca8764d48d213fb1495
Legimmort82 <legendlegendery1212@gmail.com>
2023-10-27 18:50:03 -0700

    we make it work bro

---
+++ src/pages/Cardpage.jsx
@@ -0,0 +1,14 @@
+import Bord from "@/components/Bord";
+import React, { useState } from "react";
+const Cardpage = () => {
+  return (
+    <div className="flex gap-3">
+      <Bord />
+      <Bord />
+      <Bord />
+      <Bord />
+    </div>
+  );
+};
+
+export default Cardpage;
