616f75bfb64cddc4ac3c8f2bbe68b4aad51df2ba
Legimmort82 <legendlegendery1212@gmail.com>
2023-10-28 18:47:48 -0700

    some changes

--- src/components/Priority/SinglePriority.jsx
+++ src/components/Priority/SinglePriority.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-const SinglePriority = ({ color }) => {
+const SinglePriority = ({ color,onClick}) => {
   const colors = {
     blue: "bg-blue-100",
     red: "bg-red-100",
     purple: "bg-purple-100",
     green: "bg-green-100",
   };
-  return <div className={`w-[60px] h-2 rounded ${colors[color]}`}></div>;
+  return (
+    <button onClick={onClick}>
+      <div className={`w-[60px] h-2 rounded ${colors[color]}`}></div>
+    </button>
+  );
 };
 
 export default SinglePriority;
